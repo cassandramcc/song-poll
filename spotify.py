f73988dc5e4cafc5895318f3cd03dc265eb0a827
Cass <cassandra.m.mccormack@gmail.com>
2023-03-06 21:38:30 +0000

    spotify api

--- spotify.py
+++ spotify.py
@@ -1,9 +1,51 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",
-                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+import config.keys as keys
+from datetime import datetime
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=keys.clientID,
+                                                           client_secret=keys.clientSecret))
+
+class Album:
+    def __init__(self, name, releaseDate):
+        self.name = name
+        self.releaseDate = releaseDate
+
+    def __str__(self):
+        return f"{self.name}-{self.releaseDate}"
+
+def getArtistID(artist):
+    searchResult = sp.search(artist,1,type='artist')
+    id = searchResult['artists']['items'][0]['id']
+    return id
+
+def getArtistAlbums(id):
+    moreAlbums = True
+    offset = 0
+    albums = []
+    while moreAlbums:
+        albumsResult = sp.artist_albums(id,limit=50,offset=offset)
+        if len(albumsResult['items']) == 0:
+            break
+        for _, album in enumerate(albumsResult['items']):
+            albums.append(Album(album['name'],album['release_date']))
+        offset += 50
+    return albums
+
+def sortAlbums(albums):
+    sortedAlbums = sorted(albums, key=lambda a: a.releaseDate, reverse=True)
+    return sortedAlbums
+
+def getAlbumsAfterVisitDate(albums,visitDate):
+    newAlbums = []
+    for album in albums:
+        if datetime.strptime(album.releaseDate,'%Y-%m-%d') >= visitDate:
+            newAlbums.append(album)
+    return newAlbums
+
+artistID = getArtistID("IID")
+albums =  getArtistAlbums("3GZ8Rfap7VxAOzABiZEXCL")
+newAlbums = getAlbumsAfterVisitDate(albums,datetime.strptime('2023-01-01','%Y-%m-%d'))
+for a in newAlbums:
+    print(a)    
